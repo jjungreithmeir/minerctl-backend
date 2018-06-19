c1898130bbfd8a47ad02174d363100421f3f6917
Jakob Jungreithmeir <jakob.jungreithmeir@bittex.at>
2018-06-19 09:08:31 +0200

    add all GET unit test cases
    All GET requests are now tested via pytest unit test cases. The JSON responses are validated against multiple JSON schemata.

--- testing/tests.py
+++ testing/tests.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import random
 from assertions import assert_valid_schema
 import pytest
 
@@ -17,6 +18,27 @@ def test_get_temperature(session):
 def test_get_filtration(session):
     resp = session.get(CONNECTION + '/filter').json()
     assert_valid_schema(resp, 'filter.json')
+
+def test_get_ventilation(session):
+    resp = session.get(CONNECTION + '/fans').json()
+    assert_valid_schema(resp, 'fans.json')
+
+def test_get_operation(session):
+    resp = session.get(CONNECTION + '/mode').json()
+    assert_valid_schema(resp, 'operation.json')
+
+def test_get_PID(session):
+    resp = session.get(CONNECTION + '/pid').json()
+    assert_valid_schema(resp, 'pid.json')
+
+def test_get_PID(session):
+    resp = session.get(CONNECTION + '/cfg').json()
+    assert_valid_schema(resp, 'config.json')
+
+def test_get_miner(session):
+    resp = session.get(CONNECTION + '/miner/' + \
+                       str(random.randint(0, 120))).json()
+    assert_valid_schema(resp, 'miner_controller.json')
 
 @pytest.fixture
 def session():
