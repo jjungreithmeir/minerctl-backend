b28e3861e77c5437f7113e3818f3bd59db401f60
Jakob Jungreithmeir <jakob.jungreithmeir@bittex.at>
2018-07-19 10:17:15 +0200

    add wrapper for mc
    The mock controller has been swapped out for the real wrapper of the microcontroller. At this stage all unit tests are passing.

--- controller.py
+++ controller.py
@@ -6,10 +6,10 @@ endpoints one simply has to add the resource in the `prepare_app` function
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token
-from mocking.mock_controller import Controller
+from src.mc_wrapper import Microcontroller
 from src.config_reader import ConfigReader
 
-MOCK = Controller()
+MC = Microcontroller()
 APP = Flask(__name__)
 APP.config['JWT_ALGORITHM'] = 'RS256'
 CFG_RDR = ConfigReader(path='config/config.ini')
@@ -23,68 +23,67 @@ PARSER = reqparse.RequestParser()
 class Info(Resource):
     @jwt_required
     def get(self):
-        return {'firmware_version': MOCK.info_fw_version}
+        return {'firmware_version': MC.info_fw_version}
 
 class Temperature(Resource):
     @jwt_required
     def get(self):
-        MOCK.randomize_variables()
-        return {'measurements': MOCK.temp_measurements,
-                'target': MOCK.temp_target,
-                'sensor_id': MOCK.temp_sensor_id,
-                'external': MOCK.temp_external}
+        return {'measurements': MC.temp_measurements,
+                'target': MC.temp_target,
+                'sensor_id': MC.temp_sensor_id,
+                'external': MC.temp_external}
     def patch(self):
         args = PARSER.parse_args()
-        MOCK.temp_target = args['target']
-        MOCK.temp_sensor_id = args['sensor_id']
-        MOCK.temp_external = args['external']
+        MC.temp_target = args['target']
+        MC.temp_sensor_id = args['sensor_id']
+        MC.temp_external = args['external']
         return '', 200
 
 class Filtration(Resource):
     @jwt_required
     def get(self):
-        MOCK.randomize_variables()
-        return {'pressure_diff': MOCK.filter_pressure_diff,
-                'status_ok': MOCK.filter_status_ok,
-                'threshold': MOCK.filter_threshold}
+
+        return {'pressure_diff': MC.filter_pressure_diff,
+                'status_ok': MC.filter_status_ok,
+                'threshold': MC.filter_threshold}
     def patch(self):
         args = PARSER.parse_args()
-        MOCK.filter_threshold = args['threshold']
+        MC.filter_threshold = args['threshold']
         return '', 200
 
 class Ventilation(Resource):
     @jwt_required
     def get(self):
-        MOCK.randomize_variables()
-        return {'min_rpm': MOCK.fans_min_rpm,
-                'max_rpm': MOCK.fans_max_rpm,
-                'rpm': MOCK.fans_rpm}
+
+        return {'min_rpm': MC.fans_min_rpm,
+                'max_rpm': MC.fans_max_rpm,
+                'rpm': MC.fans_rpm}
 
     def patch(self):
         args = PARSER.parse_args()
-        MOCK.fans_min_rpm = args['min_rpm']
-        MOCK.fans_max_rpm = args['max_rpm']
+        MC.fans_min_rpm = args['min_rpm']
+        MC.fans_max_rpm = args['max_rpm']
         return '', 200
 
 class Operation(Resource):
     @jwt_required
     def get(self):
-        resp = {'active_mode': MOCK.active_mode}
-        if MOCK.active_mode == 'gpu':
-            resp['ontime'] = MOCK.op_gpu_ontime
-            resp['offtime'] = MOCK.op_gpu_offtime
-        elif MOCK.active_mode == 'asic':
-            resp['restime'] = MOCK.op_asic_restime
+        resp = {'active_mode': MC.active_mode}
+        if MC.active_mode == 'gpu':
+            resp['ontime'] = MC.op_gpu_ontime
+            resp['offtime'] = MC.op_gpu_offtime
+        elif MC.active_mode == 'asic':
+            resp['restime'] = MC.op_asic_restime
         return resp
 
     def patch(self):
         args = PARSER.parse_args()
-        MOCK.active_mode = args['active_mode']
-        if MOCK.active_mode == 'gpu':
-            MOCK.op_gpu_ontime = args['ontime']
-            MOCK.op_gpu_offtime = args['offtime']
-        if MOCK.active_mode == 'asic':
-            MOCK.op_asic_restime = args['restime']
+        MC.active_mode = args['active_mode']
+        if MC.active_mode == 'gpu':
+            MC.op_gpu_ontime = args['ontime']
+            MC.op_gpu_offtime = args['offtime']
+        if MC.active_mode == 'asic':
+            MC.op_asic_restime = args['restime']
         return '', 200
 
 class MinerController(Resource):
@@ -92,7 +91,7 @@ class MinerController(Resource):
     def get(self):
         args = PARSER.parse_args()
         miner_id = int(args['id'])
-        return {'running': MOCK.miners[miner_id]}
+        return {'running': MC.miners[miner_id]}
 
     def patch(self):
         args = PARSER.parse_args()
@@ -100,13 +99,13 @@ class MinerController(Resource):
         miner_id = args['id']
         if action == 'on' or action == 'register':
             # new miners are turned on by default
-            MOCK.miners[miner_id] = True
+            MC.miners[miner_id] = True
         elif action == 'off':
-            MOCK.miners[miner_id] = False
+            MC.miners[miner_id] = False
         elif action == 'toggle':
-            MOCK.miners[miner_id] = not MOCK.miners[miner_id]
+            MC.miners[miner_id] = not MC.miners[miner_id]
         elif action == 'deregister':
-            MOCK.miners[miner_id] = None
+            MC.miners[miner_id] = None
         elif action == 'reset':
             # TODO reset action
             pass
@@ -118,16 +117,16 @@ class MinerController(Resource):
 class PID(Resource):
     @jwt_required
     def get(self):
-        return {'proportional': MOCK.pid_proportional,
-                'integral': MOCK.pid_integral,
-                'derivative': MOCK.pid_derivative,
-                'bias': MOCK.pid_bias}
+        return {'proportional': MC.pid_proportional,
+                'integral': MC.pid_integral,
+                'derivative': MC.pid_derivative,
+                'bias': MC.pid_bias}
     def put(self):
         args = PARSER.parse_args()
-        MOCK.pid_proportional = args['proportional']
-        MOCK.pid_integral = args['integral']
-        MOCK.pid_derivative = args['derivative']
-        MOCK.pid_bias = args['bias']
+        MC.pid_proportional = args['proportional']
+        MC.pid_integral = args['integral']
+        MC.pid_derivative = args['derivative']
+        MC.pid_bias = args['bias']
         return '', 200
 
 class Config(Resource):
@@ -138,44 +137,44 @@ class Config(Resource):
     """
     @jwt_required
     def get(self):
-        MOCK.randomize_variables()
-        return {'fw_version': MOCK.info_fw_version,
-                'measurements': MOCK.temp_measurements,
-                'target': MOCK.temp_target,
-                'sensor_id': MOCK.temp_sensor_id,
-                'external': MOCK.temp_external,
-                'pressure_diff': MOCK.filter_pressure_diff,
-                'status_ok': MOCK.filter_status_ok,
-                'threshold': MOCK.filter_threshold,
-                'min_rpm': MOCK.fans_min_rpm,
-                'max_rpm': MOCK.fans_max_rpm,
-                'rpm': MOCK.fans_rpm,
-                'active_mode': MOCK.active_mode,
-                'proportional': MOCK.pid_proportional,
-                'integral': MOCK.pid_integral,
-                'derivative': MOCK.pid_derivative,
-                'bias': MOCK.pid_bias,
-                'ontime': MOCK.op_gpu_ontime,
-                'offtime': MOCK.op_gpu_offtime,
-                'restime': MOCK.op_asic_restime,
-                'miners': MOCK.miners}
+
+        return {'fw_version': MC.info_fw_version,
+                'measurements': MC.temp_measurements,
+                'target': MC.temp_target,
+                'sensor_id': MC.temp_sensor_id,
+                'external': MC.temp_external,
+                'pressure_diff': MC.filter_pressure_diff,
+                'status_ok': MC.filter_status_ok,
+                'threshold': MC.filter_threshold,
+                'min_rpm': MC.fans_min_rpm,
+                'max_rpm': MC.fans_max_rpm,
+                'rpm': MC.fans_rpm,
+                'active_mode': MC.active_mode,
+                'proportional': MC.pid_proportional,
+                'integral': MC.pid_integral,
+                'derivative': MC.pid_derivative,
+                'bias': MC.pid_bias,
+                'ontime': MC.op_gpu_ontime,
+                'offtime': MC.op_gpu_offtime,
+                'restime': MC.op_asic_restime,
+                'miners': MC.miners.serialize()}
 
     def patch(self):
         args = PARSER.parse_args()
-        MOCK.temp_target = args['target']
-        MOCK.temp_sensor_id = args['sensor_id']
-        MOCK.temp_external = args['external']
-        MOCK.filter_threshold = args['threshold']
-        MOCK.fans_min_rpm = args['min_rpm']
-        MOCK.fans_max_rpm = args['max_rpm']
-        MOCK.active_mode = args['active_mode']
-        MOCK.pid_proportional = args['proportional']
-        MOCK.pid_integral = args['integral']
-        MOCK.pid_derivative = args['derivative']
-        MOCK.pid_bias = args['bias']
-        MOCK.op_gpu_ontime = args['ontime']
-        MOCK.op_gpu_offtime = args['offtime']
-        MOCK.op_asic_restime = args['restime']
+        MC.temp_target = args['target']
+        MC.temp_sensor_id = args['sensor_id']
+        MC.temp_external = args['external']
+        MC.filter_threshold = args['threshold']
+        MC.fans_min_rpm = args['min_rpm']
+        MC.fans_max_rpm = args['max_rpm']
+        MC.active_mode = args['active_mode']
+        MC.pid_proportional = args['proportional']
+        MC.pid_integral = args['integral']
+        MC.pid_derivative = args['derivative']
+        MC.pid_bias = args['bias']
+        MC.op_gpu_ontime = args['ontime']
+        MC.op_gpu_offtime = args['offtime']
+        MC.op_asic_restime = args['restime']
         return '', 200
 
 def prepare_app():
---
+++ src/mc_wrapper.py
@@ -0,0 +1,140 @@
+import serial
+import time
+
+SET_ACTIONS = {None: -1, False: 0, True: 1}
+GET_ACTIONS = {SET_ACTIONS[None]: None,
+               SET_ACTIONS[False]: False,
+               SET_ACTIONS[True]: True}
+CMD_DICT = {
+    'info_fw_version': 'fw',
+    'temp_target': 'targettemp',
+    'temp_sensor_id': 'sensor',
+    'temp_external': 'external',
+    'filter_pressure_diff': 'pressure',
+    'filter_status_ok': 'filter', # TODO this could be a potential error source as it was a bool once
+    'filter_threshold': 'threshold',
+    'fans_min_rpm': 'minrpm',
+    'fans_max_rpm': 'maxrpm',
+    'fans_rpm': 'rpm',
+    '_max_number_of_miners': 'maxminers',
+    'pid_proportional': 'pidp',
+    'pid_integral': 'pidi',
+    'pid_derivative': 'pidd',
+    'pid_bias': 'pidb',
+    'op_gpu_ontime': 'ontime',
+    'op_gpu_offtime': 'offtime',
+    'op_asic_restime': 'restime'
+}
+
+class MinerIterator:
+    def __init__(self, miners):
+        self._miners = miners
+        self._index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self._index >= len(self._miners):
+            raise StopIteration
+        val = GET_ACTIONS[self._miners[self._index]]
+        self._index += 1
+        return val
+
+class MinerList:
+    def __init__(self, length):
+        self._length = int(_read("?maxminers"))
+    def __len__(self):
+        return self._length
+    def __getitem__(self, key):
+        state = int(_read("?miner {}".format(key)))
+        return GET_ACTIONS[state]
+    def __setitem__(self, key, value):
+        print("!miner {} {}".format(key, SET_ACTIONS[value]))
+        _write("!miner {} {}".format(key, SET_ACTIONS[value]))
+    def __iter__(self):
+        #TODO fix iterator
+        return MinerIterator(self._miners).__iter__()
+    def serialize(self):
+        states = []
+        for miner in range(self._length):
+            state = int(_read("?miner {}".format(miner)))
+            states.append(GET_ACTIONS[state])
+        return states
+
+# TODO change parameters based on config file
+SERIAL = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+
+def _read(cmd):
+    _write(cmd)
+    while not SERIAL.in_waiting:
+        time.sleep(.1)
+    return SERIAL.read(SERIAL.in_waiting).decode('utf-8').strip()
+
+def _write(cmd):
+    resp_len = SERIAL.write(bytes(cmd + '\n', encoding='utf-8'))
+    return resp_len
+
+class Microcontroller:
+    def __init__(self):
+        self.info_fw_version = None
+        self.temp_measurements = None
+        self.temp_target = None
+        self.temp_sensor_id = None
+        self.temp_external = None
+        self.filter_pressure_diff = None
+        self.filter_status_ok = None
+        self.filter_threshold = None
+        self.fans_min_rpm = None
+        self.fans_max_rpm = None
+        self.fans_rpm = None
+        self.active_mode = None
+        # TODO correctly init this
+        self._max_number_of_miners = 120
+        self.miners = MinerList(120)
+
+        self.pid_proportional = None
+        self.pid_integral = None
+        self.pid_derivative = None
+        self.pid_bias = None
+        self.op_gpu_ontime = None
+        self.op_gpu_offtime = None
+        self.op_asic_restime = None
+
+    def __setattr__(self, name, value):
+        if value is None or name.startswith("_"):
+            return None
+        if name == "active_mode":
+            mode_id = None
+            if value == "gpu":
+                mode_id = 0
+            elif value == "asic":
+                mode_id = 1
+            _write("!mode {}".format(mode_id))
+        elif name == "miners":
+            object.__setattr__(self, name, value)
+            # TODO add direct access to miners
+        else:
+            _write("!{} {}".format(CMD_DICT[name], value))
+
+    def __getattribute__(self, name):
+        if name == "temp_measurements":
+            # original format: 0:15, 1:22, 2:33
+            measurements = _read("?temps")
+            output = dict()
+            for items in measurements.split(", "):
+                temp = items.split(":")
+                output[int(temp[0])] = temp[1]
+            return output
+        elif name == "active_mode":
+            mode_id = int(_read("?mode"))
+            modes = ["gpu", "asic"]
+            return modes[mode_id]
+        elif name == "miners":
+            return object.__getattribute__(self, name)
+        elif name == "info_fw_version":
+            return _read("?{}".format(name))
+        elif name == "filter_status_ok":
+            return bool(_read("?{}".format(name)))
+        else:
+            return int(_read("?{}".format(CMD_DICT[name])))
